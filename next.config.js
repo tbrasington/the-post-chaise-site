a5de54aa0b88efb41df892b02df04cf4685ba73b
Thomas Brasington <tom@2c2d.co.uk>
2021-07-28 13:49:40 +0100

    make analazye work

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const withPlugins = require("next-compose-plugins")
+
 const commerce = require("./commerce.config.json")
 const {
   withCommerceConfig,
@@ -28,43 +30,49 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true"
 })
 
-module.exports = withBundleAnalyzer(
-  withSentryConfig(
-    withCommerceConfig(
-      {
-        commerce,
-        i18n: {
-          locales: ["en-GB"],
-          defaultLocale: "en-GB"
-        },
-        images: {
-          domains: ["cdn.sanity.io"]
-        },
-        rewrites() {
-          return [
-            (isBC || isShopify || isSwell || isVendure) && {
-              source: "/checkout",
-              destination: "/api/checkout"
-            },
-            // The logout is also an action so this route is not required, but it's also another way
-            // you can allow a logout!
-            isBC && {
-              source: "/logout",
-              destination: "/api/logout?redirect_to=/"
-            },
-            // For Vendure, rewrite the local api url to the remote (external) api url. This is required
-            // to make the session cookies work.
-            isVendure &&
-              process.env.NEXT_PUBLIC_VENDURE_LOCAL_URL && {
-                source: `${process.env.NEXT_PUBLIC_VENDURE_LOCAL_URL}/:path*`,
-                destination: `${process.env.NEXT_PUBLIC_VENDURE_SHOP_API_URL}/:path*`
-              }
-          ].filter(Boolean)
-        }
+// module.exports = withBundleAnalyzer(
+//   withSentryConfig(withCommerceConfig(SentryWebpackPluginOptions))
+// )
+
+module.exports = withPlugins([
+  [withBundleAnalyzer],
+  [withSentryConfig, SentryWebpackPluginOptions],
+  [
+    withCommerceConfig,
+    {
+      commerce,
+      i18n: {
+        locales: ["en-GB"],
+        defaultLocale: "en-GB"
       },
-      SentryWebpackPluginOptions
-    )
-  )
-)
+      images: {
+        domains: ["cdn.sanity.io"]
+      },
+      rewrites() {
+        return [
+          (isBC || isShopify || isSwell || isVendure) && {
+            source: "/checkout",
+            destination: "/api/checkout"
+          },
+          // The logout is also an action so this route is not required, but it's also another way
+          // you can allow a logout!
+          isBC && {
+            source: "/logout",
+            destination: "/api/logout?redirect_to=/"
+          },
+          // For Vendure, rewrite the local api url to the remote (external) api url. This is required
+          // to make the session cookies work.
+          isVendure &&
+            process.env.NEXT_PUBLIC_VENDURE_LOCAL_URL && {
+              source: `${process.env.NEXT_PUBLIC_VENDURE_LOCAL_URL}/:path*`,
+              destination: `${process.env.NEXT_PUBLIC_VENDURE_SHOP_API_URL}/:path*`
+            }
+        ].filter(Boolean)
+      }
+    }
+  ]
+  // your other plugins here
+])
+
 // Don't delete this console log, useful to see the commerce config in Vercel deployments
 console.log("next.config.js", JSON.stringify(module.exports, null, 2))
