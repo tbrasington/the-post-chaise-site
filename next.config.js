6fec10a8be3ae3ad9506ba26c34ba12d276b65bc
Thomas Brasington <tbrasington@gmail.com>
2021-10-31 23:15:05 +0000

    sentry

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
-const withPlugins = require("next-compose-plugins")
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 const { withSentryConfig } = require("@sentry/nextjs")
 
-const SentryWebpackPluginOptions = {
+const moduleExports = {
+  i18n: {
+    locales: ["en-GB"],
+    defaultLocale: "en-GB"
+  },
+  images: {
+    domains: ["cdn.sanity.io"],
+    loader: "custom"
+  }
+}
+
+const sentryWebpackPluginOptions = {
   // Additional config options for the Sentry Webpack plugin. Keep in mind that
   // the following options are set automatically, and overriding them is not
   // recommended:
@@ -14,30 +28,6 @@ const SentryWebpackPluginOptions = {
   // https://github.com/getsentry/sentry-webpack-plugin#options.
 }
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true"
-})
-
-// module.exports = withBundleAnalyzer(
-//   withSentryConfig(withCommerceConfig(SentryWebpackPluginOptions))
-// )
-
-module.exports = withPlugins([
-  [
-    {
-      i18n: {
-        locales: ["en-GB"],
-        defaultLocale: "en-GB"
-      },
-      images: {
-        domains: ["cdn.sanity.io"],
-        loader: "custom"
-      }
-    }
-  ],
-  [withBundleAnalyzer],
-  [withSentryConfig, SentryWebpackPluginOptions]
-])
-
-// Don't delete this console log, useful to see the commerce config in Vercel deployments
-console.log("next.config.js", JSON.stringify(module.exports, null, 2))
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions)
---
+++ next.config.sample.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    i18n: {
+      locales: ["en-GB"],
+      defaultLocale: "en-GB"
+    },
+    images: {
+      domains: ["cdn.sanity.io"],
+      loader: "custom"
+    }
+  }
+]
