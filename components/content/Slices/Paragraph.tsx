6bda0f9f5ba4a66b0dab66af0b0da08676fe3ba9
Thomas Brasington <tbrasington@gmail.com>
2021-12-03 18:56:21 +0000

    Update Paragraph.tsx

--- components/content/Slices/Paragraph.tsx
+++ components/content/Slices/Paragraph.tsx
@@ -2,7 +2,11 @@ import { Box } from "theme-ui"
 import { FC } from "react"
 import { PageContent } from "@sanityLib/types/guides"
 import { PortableText } from "@sanityLib/sanity"
-import { TextStyleNames } from "@theme/tokens"
+import {
+  StandardLeftIndent,
+  StandardXPadding,
+  TextStyleNames
+} from "@theme/tokens"
 
 interface ParagraphProps {
   content: PageContent
@@ -13,8 +17,9 @@ const Paragraph: FC<ParagraphProps> = ({ content }) => {
     <Box
       sx={{
         variant: `text.${TextStyleNames.paragraph}`,
-        maxWidth: "56ch",
-        margin: "auto"
+
+        ml: StandardLeftIndent,
+        maxWidth: "56ch"
       }}
     >
       <PortableText blocks={content.body} />
