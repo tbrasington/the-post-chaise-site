c73159d169f297ded1afd738d736b1d0ee2ac962
Thomas Brasington <mail@tbrasington.com>
2024-02-23 14:01:27 +0000

    add media plugin to sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,18 +1,19 @@
 /**
  * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...index]]/page.tsx` route
  */
+import { media } from "sanity-plugin-media";
 
-import { visionTool } from '@sanity/vision'
-import { defineConfig } from 'sanity'
-import { structureTool } from 'sanity/structure'
-import { presentationTool } from 'sanity/presentation'
+import { visionTool } from "@sanity/vision";
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { presentationTool } from "sanity/presentation";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import { apiVersion, dataset, projectId } from './src/sanity/env'
-import { schema } from './src/sanity/schema'
+import { apiVersion, dataset, projectId } from "./src/sanity/env";
+import { schema } from "./src/sanity/schema";
 
 export default defineConfig({
-  basePath: '/studio',
+  basePath: "/studio",
   projectId,
   dataset,
   // Add and edit the content schema in the './sanity/schema' folder
@@ -25,9 +26,10 @@ export default defineConfig({
     presentationTool({
       previewUrl: {
         draftMode: {
-          enable: '/api/draft',
+          enable: "/api/draft",
         },
       },
     }),
+    media(),
   ],
-})
+});
