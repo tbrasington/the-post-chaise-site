2e5141554375f093b127622e857b0bddbf9e30ab
Thomas Brasington <mail@tbrasington.com>
2024-02-23 11:07:57 +0000

    footer and colophn

---
+++ src/app/(site)/colophon/page.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+
+export default function Colophon() {
+  return (
+    <main className="prose prose-stone dark:prose-invert prose-base container mx-auto  space-y-7 px-6 py-11">
+      <h1 className="m-0 text-sm font-medium antialiased">Colophon</h1>
+      <div>
+        <h2 className="m-0  text-sm  font-medium antialiased">What</h2>
+        <p className="m-0 mt-2 text-sm  antialiased">
+          A photographic archive by{" "}
+          <Link href="https://tbrasington.com">tbrasington</Link>. Pre
+          social-networks (around 2004-8) this is what I and most of used to do
+          with our photos. This is a bit of a digital garden for me to test out
+          other technology and design combinations away from my work web
+          prescene.
+        </p>
+      </div>
+      <div>
+        <h2 className="m-0  text-sm  font-medium antialiased">Typography</h2>
+        <p className="m-0 mt-2 text-sm  antialiased">
+          Set in <Link href="https://vercel.com/font/sans">Geist Sans</Link> and{" "}
+          <Link href="https://vercel.com/font/mono">Geist Mono</Link> by Vercel.
+          It has nice proportions for small text, and the niceities of classic
+          grotesques and some of the more humanist/softer touchers in modern
+          sans.
+        </p>
+      </div>
+      <div>
+        <h2 className="m-0  text-sm  font-medium antialiased">Technology</h2>
+        <p className="m-0 mt-2 text-sm  antialiased">
+          Written in React via <Link href="https://nextjs.org">Next.js</Link>{" "}
+          with <Link href="https://sanity.io">Sanity</Link> providing the CMS.
+          Styling is with <Link href="https://tailwindcss.com">Tailwind</Link>,
+          which works really well for this kind of site as it lets you focus on
+          the layout. Hosted on <Link href="https://vercel.com">Vercel</Link>.
+          Analytics by privacy focussed{" "}
+          <Link href="https://plausible.io">Plausible</Link>
+        </p>
+      </div>
+    </main>
+  );
+}
--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
+import PlausibleProvider from "next-plausible";
 
 import "./globals.css";
 import LiveVisualEditing from "@/components/live-visual-editing";
@@ -20,25 +21,33 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
-      <body
-        className={`${GeistSans.variable} ${GeistMono.variable} bg-stone font-sans text-sm dark:bg-stone-950 dark:text-stone-400`}
-      >
-        <div className="flex flex-col gap-4">
-          <header className="container m-auto flex items-center justify-between gap-4 px-4 py-10 md:pt-20">
-            <Link
-              href="/"
-              className="font-mono text-sm uppercase tracking-widest "
-            >
-              The Post Chaise
-            </Link>
-            <Logo />
-          </header>
+    <PlausibleProvider domain="thepostchaise.com">
+      <html lang="en" className="scroll-smooth">
+        <body
+          className={`${GeistSans.variable} ${GeistMono.variable} bg-stone font-sans text-sm dark:bg-stone-950 dark:text-stone-400`}
+        >
+          <div className="flex flex-col gap-4">
+            <header className="container m-auto flex items-center justify-between gap-4 px-4 py-10 md:pt-20">
+              <Link
+                href="/"
+                className="font-mono text-sm uppercase tracking-widest "
+              >
+                The Post Chaise
+              </Link>
+              <Logo />
+            </header>
 
-          {children}
-          {draftMode().isEnabled && <LiveVisualEditing />}
-        </div>
-      </body>
-    </html>
+            {children}
+            {draftMode().isEnabled && <LiveVisualEditing />}
+            <footer className="container mx-auto flex justify-between gap-4 px-4 py-20 text-stone-500">
+              <Link href="/colophon" className="text-stone-500">
+                Colophon
+              </Link>{" "}
+              <span>© {new Date().getFullYear()} Thomas Brasington</span>
+            </footer>
+          </div>
+        </body>
+      </html>
+    </PlausibleProvider>
   );
 }
